ec7c4d87c7fd9118ee2df03000c2731c5119a2a8
naima-mohammed <n.aldasoqi@uot.edu.ly>
2026-04-17 16:11:54 +0200

    update

--- main.cpp
+++ main.cpp
@@ -1,82 +1,84 @@
 #include "search.h"
 #include <iostream>
-
+#include <fstream>// used for output file
+#include <cstdlib>// used for random number generation
+#include <ctime>// used for random number generation
 using namespace std;
 
-int main() {
+int main()
+ {
+     ofstream out("output.txt");
      int num_algorithm;
-    State goal;
-    
+     State goal;
+     
     goal.agent_pos = {1,1};// الهدف هو الوصول إلى (1,1)
     goal.c1 = true;
     goal.c2 = true;
     goal.c3 = true;
     goal.c4 = true;
 
+    srand(time(0)); // used to seed the random number generator with the current time, ensuring different random values each time the program is run
+   
+    // coins random
+    for(int i = 0; i < 4; i++)
+     {
+    coins[i].row = rand() % 10 + 1;
+    coins[i].col = rand() % 10 + 1;
+    }
+
+    // fuel place random
+    fuel_place.row = rand() % 10 + 1;
+    fuel_place.col = rand() % 10 + 1;
 
    // goal.collected_coins = 15; // 1111 in binary, meaning all coins collected
    // tests[i] = {{x,y}, fuel, false, false, false, false};
-    State tests[10];// مجموعة من الحالات الابتدائية للtests 
+   //Represent the tests in an array of type State, each test containing a different location for the (x,y) agent, a different amount of fuel, and coins with a value of 0.
+   // { {tests[i].agent_pos.row, tests[i].agent_pos.col}, tests[i].fuel, tests[i].collected_coins }
+    
+
+   State tests[10];// مجموعة من الحالات الابتدائية للtests 
+   for(int i = 0; i < 10; i++)
+    {
+
+    tests[i].agent_pos.row = rand() % 10 + 1; // من 1 إلى 10
+    tests[i].agent_pos.col = rand() % 10 + 1;
+
+    tests[i].fuel = rand() % 20 + 1; // من 1 إلى 20
+
+    tests[i].c1 = false;
+    tests[i].c2 = false;
+    tests[i].c3 = false;
+    tests[i].c4 = false;
+}
 
 
-     //Represent the tests in an array of type State, each test containing a different location for the (x,y) agent, a different amount of fuel, and coins with a value of 0.
-     // { {tests[i].agent_pos.row, tests[i].agent_pos.col}, tests[i].fuel, tests[i].collected_coins }
-    /*tests[0] = {{9,9}, 10, 0};
-    tests[1] = {{5,5}, 8, 0};
-    tests[2] = {{3,7}, 12, 0};
-    tests[3] = {{4,6}, 15, 0};
-    tests[4] = {{2,2}, 5, 0};
-    tests[5] = {{1,2}, 5, 0};     
-    tests[6] = {{10,10}, 3, 0};  
-    tests[7] = {{4,8}, 1, 0};    
-    tests[8] = {{7,7}, 20, 0};   
-    tests[9] = {{9,2}, 2, 0}; 
-  */
+  out<<"**********************************************BFS************************************************\n";
+    for(int i = 0; i < 10; i++)
+     {  
+        out << "\n****** test " << i+1 << " ********\n";
+        out << "Start State: ("<< tests[i].agent_pos.row << ","<< tests[i].agent_pos.col << ", fuel:" << tests[i].fuel << ")\n";
+        BFS(tests[i], goal, out); out << endl;
+     }
+ 
 
-
-   tests[0] = {{5,7}, 20, false, false, false, false};
-   tests[1] = {{5,5}, 8, false, false, false, false};
-   tests[2] = {{3,7}, 12, false, false, false, false};
-   tests[3] = {{4,6}, 15, false, false, false, false};
-   tests[4] = {{2,2}, 5, false, false, false, false};
-   tests[5] = {{1,2}, 5, false, false, false, false};
-   tests[6] = {{10,10}, 3, false, false, false, false};
-   tests[7] = {{4,8}, 1, false, false, false, false};
-   tests[8] = {{7,7}, 20, false, false, false, false};
-   tests[9] = {{9,2}, 2, false, false, false, false};
-   
-
-
-   // display
- cout << "1-BFS, 2-DFS, 3-IDS\n";
- cout << "Enter the number of the search algorithm you want to use: "; cin >> num_algorithm;
-
-  if(num_algorithm == 1) 
-  {
+  out<<"**********************************************DFS************************************************\n";
     for(int i = 0; i < 10; i++)
      {
-        cout << "\n****** test " << i+1 << " ********\n";
-        cout << "Start State: ("<< tests[i].agent_pos.row << ","<< tests[i].agent_pos.col << ", fuel:" << tests[i].fuel << ")\n";
-        BFS(tests[i], goal); cout << endl;
+        out << "\n****** test " << i+1 << " ********\n";
+        out << "Start State: ("<< tests[i].agent_pos.row << ","<< tests[i].agent_pos.col<< ", fuel:" << tests[i].fuel << ")\n";
+        DFS(tests[i], goal, out);out << endl;
      }
-   }
- else if(num_algorithm == 2)
- {
-    for(int i = 0; i < 10; i++) {
-        cout << "\n****** test " << i+1 << " ********\n";
-        cout << "Start State: ("<< tests[i].agent_pos.row << ","<< tests[i].agent_pos.col<< ", fuel:" << tests[i].fuel << ")\n";
-        DFS(tests[i], goal);cout << endl;
-    }
-  }
- else if(num_algorithm == 3)
- {
-    for(int i = 0; i < 10; i++) {
-        cout << "\n****** test " << i+1 << " ********\n";
-        cout << "Start State: ("<< tests[i].agent_pos.row << ","<< tests[i].agent_pos.col << ", fuel:" << tests[i].fuel << ")\n";
-        IDS(tests[i], goal); cout  << endl;
-    }
- }
-  else {cout << "invalid choice!\n";}
+ 
 
+  out<<"**********************************************IDS************************************************\n";
+    for(int i = 0; i < 10; i++) 
+    {
+        out << "\n****** test " << i+1 << " ********\n";
+        out << "Start State: ("<< tests[i].agent_pos.row << ","<< tests[i].agent_pos.col << ", fuel:" << tests[i].fuel << ")\n";
+        IDS(tests[i], goal, out); out  << endl;
+    }
+
+    out.close();
     return 0;
+  
 }
